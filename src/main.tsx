0d5f85d2ecc33932b23c25c51e60431e3646430c
sakey01 <sheikh36ahmed@gmail.com>
2025-08-27 00:59:58 +0100

    deployment fixes

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <HashRouter>
+      <HashRouter basename="/e-commerce">
         <App />
       </HashRouter>
     </Provider>
